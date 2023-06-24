dcc21c8b3696ad0db446bd19fde2a80ef274940f
Steven Sun <gsuuon@gmail.com>
2023-06-23 19:41:32 -0500

    feat: black is transparent via svg filter

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,63 +1,8 @@
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/latest/tutorial/process-model
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
-
+import { contextBridge } from 'electron';
 import './index.css';
 
-window.setCircle = () => {
-  const outer = document.getElementById('outer')
-  outer.classList.remove('rectangle')
-}
-
 (async () => {
-  const devices = await navigator.mediaDevices.enumerateDevices()
-
-  const videoSources = devices.filter(d => d.kind === 'videoinput')
-
-  const select = document.createElement('select')
-
-  select.append(document.createElement('option'))
-
-  select.append(
-    ...(videoSources.map(d => {
-      const opt = document.createElement('option')
-      opt.textContent = d.label
-      opt.setAttribute('value', d.deviceId)
-      return opt
-    }))
-  )
-
-  document.body.appendChild(select)
-
-  select.onchange = (ev) => {
-    const deviceId = (ev.target as HTMLSelectElement).value
-
-    if (deviceId === '') { return }
-
+  const setDevice = deviceId => {
     const cam = document.getElementById('camera') as HTMLVideoElement
 
     navigator.mediaDevices.getUserMedia({
@@ -70,18 +15,66 @@ window.setCircle = () => {
         alert('could not connect stream');
       });
 
-    document.body.removeChild(select)
+    const toggleBlur = (() => {
+      let blurred = false
 
-    let blurred = false
+      return () => {
+        if (!blurred) {
+          const filters = window.getComputedStyle(cam).filter
+          cam.style.filter = filters + ' blur(22px)'
+        } else {
+          cam.style.filter = ''
+        }
+        blurred = !blurred
+      }
+    })()
 
     window.addEventListener('keydown', ev => {
       switch (ev.key) {
         case 'b':
-          cam.style.filter = blurred ? '' : 'blur(22px)'
-          blurred = !blurred
+          toggleBlur()
+          break
       }
     })
   }
+
+  const showSelectDevice = () => {
+    const select = document.createElement('select')
+
+    select.append(document.createElement('option'))
+
+    select.append(
+      ...(videoSources.map(d => {
+        const opt = document.createElement('option')
+        opt.textContent = d.label
+        opt.setAttribute('value', d.deviceId)
+        return opt
+      }))
+    )
+
+    document.body.appendChild(select)
+
+    select.onchange = (ev) => {
+      const deviceId = (ev.target as HTMLSelectElement).value
+
+      if (deviceId === '') { return }
+      setDevice(deviceId)
+      document.body.removeChild(select)
+    }
+  }
+
+  const devices = await navigator.mediaDevices.enumerateDevices()
+
+  const videoSources = devices.filter(d => d.kind === 'videoinput')
+
+  console.log({videoSources})
+
+  setDevice(videoSources[1].deviceId)
+
+  // showSelectDevice()
 })()
 
-
+window.setCircle = () => {
+  const outer = document.getElementById('outer')
+  outer.classList.remove('rectangle')
+}
